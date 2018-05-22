9536279820e846a6d23fb0d0a8a1970f059cf5d9
jsandoval21 <juansndvl89@gmail.com>
2018-05-21 21:35:04 -0800

    Created SingleFragmentActivity class

--- app/src/main/java/com/fatmenbrews/beerguru/MainActivity.java
+++ app/src/main/java/com/fatmenbrews/beerguru/MainActivity.java
@@ -5,21 +5,10 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if(fragment == null) {
-            fragment = new BeerFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new BeerFragment();
     }
 }
